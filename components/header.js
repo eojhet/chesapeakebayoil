c59909d042cbbd6bf324cc1b4d52bc4ddff2bce5
eojhet <eojhet@gmail.com>
2022-04-06 12:23:56 -0400

    Added footer, began writing sections

--- components/header.js
+++ components/header.js
@@ -8,6 +8,7 @@ export default function Header () {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet" /> 
+      <link rel="icon" type="image/x-icon" href="/favicon.ico?v=7" />
     </Head>
   )
 }
