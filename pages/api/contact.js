476f3f1ae8c19eaef1c43762121a37cdfc129ba1
Joe Gonzalez <eojhet@gmail.com>
2022-04-06 19:52:52 -0400

    fixed crossOrigin and anon default export

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,6 @@
 
 
-export default function (req, res) {
+export default function handler (req, res) {
   if (req.method === 'POST') {
     console.log(req.body);
     res.status(200).send({ message: 'Success'});
