353eba6521fe7014085c211f1a6c3d34c11663a9
eojhet <eojhet@gmail.com>
2022-04-06 15:03:48 -0400

    added contact us aside with basic validation

---
+++ components/contactUs.js
@@ -0,0 +1,140 @@
+import { useState, useRef } from 'react';
+import styles from '/styles/ContactUs.module.scss';
+
+export default function ContactUs () {
+  const [name, setName] = useState('');
+  const [nameWarn, setNameWarn] = useState('');
+  const [email, setEmail] = useState('');
+  const [emailWarn, setEmailWarn] = useState('');
+  const [phone, setPhone] = useState('');
+  const [phoneWarn, setPhoneWarn] = useState('');
+  const [comment, setComment] = useState('');
+  const [commentWarn, setCommentWarn] = useState('');
+  const nameInput = useRef();
+  const emailInput = useRef();
+  const phoneInput = useRef();
+  const commentInput = useRef();
+
+  const emailReg = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  const checkName = (e) => {
+    if (name.length < 3) {
+      setNameWarn("Enter a name.")
+      e.target.style.borderColor = 'red';
+    } else {
+      setNameWarn('')
+      e.target.style.borderColor = 'hsla(208, 71%, 56%, 0.8)';
+    }
+  }
+
+  const checkEmail = (e) => {
+    if (!emailReg.test(email)) {
+      setEmailWarn("Enter valid email.");
+      e.target.style.borderColor = 'red';
+    } else {
+      setEmailWarn('');
+      e.target.style.borderColor = 'hsla(208, 71%, 56%, 0.8)';
+    }
+  }
+
+  const checkPhone = (e) => {
+    if (!phone) {
+      setPhoneWarn('')
+      e.target.style.borderColor = 'hsla(208, 71%, 56%, 0.8)';
+    } else if (phone.length < 10) {
+      setPhoneWarn('Enter valid number.')
+      e.target.style.borderColor = 'red';
+    } else {
+      setPhoneWarn('')
+      e.target.style.borderColor = 'hsla(208, 71%, 56%, 0.8)';
+    }
+  }
+
+  const checkComment = (e) => {
+    if (!comment){
+      setCommentWarn("Please enter a message.")
+      e.target.style.borderColor = 'red';
+    } else {
+      setCommentWarn('')
+      e.target.style.borderColor = 'hsla(208, 71%, 56%, 0.8)';
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name.length > 2 &&
+        emailReg.test(email) &&
+        (!phone || phone.length > 9) &&
+        comment.length > 0){
+
+    } else {
+      nameInput.current.focus();
+      emailInput.current.focus();
+      phoneInput.current.focus();
+      commentInput.current.focus();
+      e.target.focus();
+    }
+  }
+
+  return (
+    <aside className={styles.aside}>
+      <form className={styles.form}>
+
+        <h1>Questions?</h1>
+
+        <label>Full Name:</label>
+        <br />
+        <input
+          name="name"
+          value={name}
+          autoComplete="name"
+          onChange={e => setName(e.target.value)}
+          onBlur={checkName}
+          ref={nameInput}
+        />
+        <br />
+        <div className={styles.warning}>{nameWarn}&nbsp;</div>
+
+        <label>Email:&nbsp;</label>
+        <br />
+        <input
+          name="email"
+          value={email}
+          autoComplete="email"
+          onChange={e => setEmail(e.target.value)}
+          onBlur={checkEmail}
+          ref={emailInput}
+        />
+        <div className={styles.warning}>{emailWarn}&nbsp;</div>
+
+        <label>Phone (optional):</label>
+        <br />
+        <input
+          name="phone"
+          value={phone}
+          autoComplete="tel"
+          placeholder='123-456-7890'
+          onChange={e => setPhone(e.target.value)}
+          onBlur={checkPhone}
+          ref={phoneInput}
+        />
+        <div className={styles.warning}>{phoneWarn}&nbsp;</div>
+
+        <label>Questions/Concerns:</label>
+        <br />
+        <textarea
+          name="comment"
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+          maxLength={1024}
+          spellCheck={true}
+          onBlur={checkComment}
+          ref={commentInput}
+        />
+        <div className={styles.warning}>{commentWarn}&nbsp;</div>
+        <br />
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+      </form>
+    </aside>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import ContactUs from '../components/contactUs'
 import Footer from '../components/footer'
 import Header from '../components/header'
 import Navbar from '../components/navbar'
@@ -13,21 +14,24 @@ export default function Home() {
       <Navbar />
       <main className={styles.container}>
         <section>
-          <h3>Home Heating Oil Tanks</h3>
-          <p>{"According to the Virginia Department of Environmental Quality (VDEQ), there are over 17,500 active petroleum underground storage tanks state wide. Home heating oil tanks are not regulated by the VDEQ but heating oil leaks are regulated. If you suspect a leak in your heating oil system, here are some steps you can take:"}</p>
-          <ul>
-            <li>Report sudden changes in oil levels to your oil supplier immediately</li>
-            <li>Use sight and smell. If you can smell petroleum vapors in your house or yard <a href="https://www.deq.virginia.gov/get-involved/about-deq/contact-us">contact the VDEQ immediately.</a></li>
-            <li>Contact a local environmental company and ask for a soil boring to determine oil has leaked into surrounding subsurface soils.</li>
-          </ul>
-        </section>
+          <article>
+            <h3>Home Heating Oil Tanks</h3>
+            <p>{"According to the Virginia Department of Environmental Quality (VDEQ), there are over 17,500 active petroleum underground storage tanks state wide. Home heating oil tanks are not regulated by the VDEQ but heating oil leaks are regulated. If you suspect a leak in your heating oil system, here are some steps you can take:"}</p>
+            <ul>
+              <li>Report sudden changes in oil levels to your oil supplier immediately</li>
+              <li>Use sight and smell. If you can smell petroleum vapors in your house or yard <a href="https://www.deq.virginia.gov/get-involved/about-deq/contact-us">contact the VDEQ immediately.</a></li>
+              <li>Contact a local environmental company and ask for a soil boring to determine oil has leaked into surrounding subsurface soils.</li>
+            </ul>
+          </article>
 
-        <section>
-          <h3>Unused Heating Oil Tanks</h3>
-          <p>{"Over the years, homes have been improved with newer heating oil systems using natural gas or electricity. More often than not, underground storage tanks are improperly abandoned, leaving the steel tanks to rot in the ground without emptying its contents. The fuel will eventually leak into the ground and potentially contaminate nearby water bodies and drinking wells."}</p>
-          <p>{"These imporperly abandoned tanks are out of sight and out of mind until it is time to sell the property. Home inspectors will typically look for signs of old heating oil tanks prior to a property transaction. If one is found, the buyer may insist that the tank be properly abandoned or at least request a soil sample to ensure that the soil has not been contaminated."}</p>
+          <article>
+            <h3>Unused Heating Oil Tanks</h3>
+            <p>{"Over the years, homes have been improved with newer heating oil systems using natural gas or electricity. More often than not, underground storage tanks are improperly abandoned, leaving the steel tanks to rot in the ground without emptying its contents. The fuel will eventually leak into the ground and potentially contaminate nearby water bodies and drinking wells."}</p>
+            <p>{"These imporperly abandoned tanks are out of sight and out of mind until it is time to sell the property. Home inspectors will typically look for signs of old heating oil tanks prior to a property transaction. If one is found, the buyer may insist that the tank be properly abandoned or at least request a soil sample to ensure that the soil has not been contaminated."}</p>
 
+          </article>
         </section>
+        <ContactUs />
       </main>
       <Footer />
     </>
