3ac2c7d5d13d2bbe6097a4b4f356c8f02fce05dd
eojhet <eojhet@gmail.com>
2022-04-07 09:17:55 -0400

    Emails can be sentt thru Contact Us

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,9 +1,40 @@
-
+import nodemailer from 'nodemailer';
 
 export default function handler (req, res) {
   if (req.method === 'POST') {
-    console.log(req.body);
+
+    const mailPort = +process.env.SMTP_PORT || 465;
+
+    const transporter = nodemailer.createTransport({
+      port: mailPort,
+      host: process.env.SMTP_HOST,
+      auth: {
+        user: process.env.EMAIL_SENDER,
+        pass: process.env.EMAIL_PASS,
+      },
+      secure: true,
+    });
+
+    const mailData = {
+      from: process.env.EMAIL_SENDER,
+      to: process.env.EMAIL_RECIPIENT,
+      subject: `New Contact ${req.body.name}`,
+      text: 'Name: ' + req.body.name + '\nEmail: ' + req.body.email + '\nPhone: ' + req.body.phone + '\nComment: ' + req.body.comment,
+    }
+
+    console.log(JSON.stringify(req.body));
+
+    transporter.sendMail(mailData, function (err, info) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(info);
+      };
+    })
+
     res.status(200).send({ message: 'Success'});
+
+
   } else {
     res.status(404).send({ message: 'No Resource Found'});
   }
