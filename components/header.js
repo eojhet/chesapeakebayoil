3649cb96c03d6dc57f98cf9a9ea7cd626479ca76
eojhet <eojhet@gmail.com>
2022-04-07 11:00:11 -0400

    charset to charSet, @media max-width 800px/space-between

--- components/header.js
+++ components/header.js
@@ -7,7 +7,7 @@ export default function Header () {
     <Head>
       <title>Home and commercial heating oil storage tank solutions for spills, leaks, removal, abatement, and fill in place questions.</title>
       <meta name="description" content="Heating oil tank spills, leaks, removals, abatements, number 2 fuel oil, petroleum smell, dead grass, fill port, vent line, subsurface contamination, flooded yard, heavy rain, Hampton Roads, Norfolk, Chesapeake, Virginia Beach, Suffolk, Newport News, Hampton, Portsmouth, Smithfield, Poquoson, Eastern Shore, Cape Charles, Eastville, Machipongo, Windsor, Franklin, Smithfield, Isle of Wight, Carrolton, York, Yorktown, Williamsburg, James City." />
-      <meta charset="UTF-8" />
+      <meta charSet="UTF-8" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet" /> 
