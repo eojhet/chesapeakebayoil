c8a2f69091f8e6d6ecab6f5e616d2088653c138b
Joe Gonzalez <eojhet@gmail.com>
2022-04-07 21:30:16 -0400

    Added 2 photos with descriptions

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import ContactUs from '../components/contactUs'
 import Footer from '../components/footer'
 import Header from '../components/header'
@@ -14,6 +12,14 @@ export default function Home() {
       <main className={styles.container}>
         <section>
           <article>
+            <h3>Aboveground and Underground Storage Tanks</h3>
+            <img
+              src='/static/tank_ast_ust.jpg'
+              alt='Above and below ground heating oil tanks'
+            />
+            <p>{"Aboveground storage tanks are most often installed following the failure of an underground storage tank. Your heating oil tank may just be the tip of the iceburg."}</p>
+          </article>
+          <article>
             <h3>Home Heating Oil Tanks</h3>
             <p>{"According to the Virginia Department of Environmental Quality (VDEQ), there are over 17,500 active petroleum underground storage tanks state wide. Home heating oil tanks are not regulated by the VDEQ but heating oil leaks are regulated. If you suspect a leak in your heating oil system, here are some steps you can take:"}</p>
             <ul>
@@ -22,7 +28,14 @@ export default function Home() {
               <li>Contact a local environmental company and ask for a soil boring to determine if oil has leaked into surrounding subsurface soils.</li>
             </ul>
           </article>
-
+          <article>
+            <h3>Dead Grass and Staining Near Tank.</h3>
+            <img
+              src='/static/tank_dead_grass.jpg'
+              alt='Photo of dead grass around fill port and vent pipe.'
+            />
+            <p>{"Pictured above is a photo taken after the overflow of an underground heating oil tank. In the bottom left of the photo you can see the fill port of the tank. In the top left is the vent line to the left of the water spigot. This vent line has rotted, allowing water to move into the tank during a heavy rain. Since oil floats on water, the oil surfaced and has contaminated the yard and crawspace. The homeowner complained of strong petroleum odors in the house."}</p>
+          </article>
           <article>
             <h3>Unused Heating Oil Tanks</h3>
             <p>{"Over the years, homes have been improved with newer heating systems using natural gas or electricity. Quite often, underground storage tanks are improperly abandoned, leaving the steel tank to rot in the ground without removing its contents. The fuel will eventually leak into the ground and potentially contaminate nearby water bodies and drinking wells."}</p>
