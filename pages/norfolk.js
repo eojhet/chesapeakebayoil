f23c1fc95414756f3b099c0ea280d88413df9cf5
eojhet <eojhet@gmail.com>
2022-04-14 14:23:58 -0400

    Added information for Norfolk and VB

--- pages/norfolk.js
+++ pages/norfolk.js
@@ -17,10 +17,29 @@ export default function Norfolk () {
       <main className={styles.container}>
         <section>
           <Link href='/' passHref><span className={styles.backButton}>&larr; Back To Home</span></Link>
-          <h1>Heating Oil Tank Information for Norfolk Virginia</h1>
+          <h1>Heating Oil Tank Information for Norfolk, Virginia</h1>
           <article>
             <h2>Permitting in the City of Norfolk</h2>
-            <p>{"This page is still in progress. We will have more city specific information for you in the very near future. In the meantime, please reach out to us via our "}<a href='#contactUs'>Contact Us</a>{" form."}</p>
+            <p>{"The City of Norfolk does not currently require a permit for "}<em>residential</em>{" underground storage tank (UST) or aboveground storage tank (AST) abatement/removal (whether removing a tank or filling it in place with sand or concrete). However, the "}
+            <a target={'_blank'} rel='noreferrer' href="https://www.norfolk.gov/619/Fire-Inspections-Permits">Norfolk Fire Marshal</a>
+            {" does require permitting and inspection for UST abatements."}</p>
+
+            <img
+              id='norfolk'
+              src='/static/city_norfolk.jpg'
+              alt='Downtown Norfolk, Virginia Skyline'
+            />
+            <small>Downtown Norfolk, Virginia Skyline.</small>
+
+            <p>{"The Norfolk Fire Marshal does not have guidelines dictating how a tank should be abated beyond those within the Internation Fire Code 2018 regulations. It is up to the homeowner and their consultant to decide as to whether a tank should be removed from the ground or filled in place with sand or concrete slurry. It is best to not remove tanks situated within 3 feet of the foundation of an on-site structure, so as to prevent the foundation and exterior walls from forming cracks. It is safest to fill tanks closer than 3 feet with a concrete slurry."}</p>
+
+            <p>{"According to the Norfolk, Virginia Code of Ordinances "}
+            <a target={'_blank'} rel='noreferrer' href="https://library.municode.com/va/norfolk/codes/code_of_ordinances?nodeId=COCI_CH17.1FISC">Section 17.1-44 (32):</a></p>
+            <blockquote>
+              {"Underground storage tanks: All abandoned storage tanks or underground storage tanks not in use for more than ninety (90) days shall be filled or removed pursuant to the protocols established by the fire official. "}
+            </blockquote>
+
+            <p>{"Feel free to peruse those sources provided for more information concerning UST and AST abatements in Norfolk."}</p>
 
             <p>{"If you have a heating oil tank that is currently not-in-use, or you suspect that your tank may be leaking fuel, we can help you find a contractor who can provide options for tank abatement at a low cost. Feel free to leave your information in the "}<a href='#contactUs'>Contact Us</a>{" form with any questions or concerns regarding your oil tank and we will be able to assist you within one business day or less."}</p>
           </article>
--- pages/virginiabeach.js
+++ pages/virginiabeach.js
@@ -17,10 +17,21 @@ export default function VirginiaBeach () {
       <main className={styles.container}>
         <section>
           <Link href='/' passHref><span className={styles.backButton}>&larr; Back To Home</span></Link>
-          <h1>Heating Oil Tank Information for Virginia Beach Virginia</h1>
+          <h1>Heating Oil Tank Information for Virginia Beach, Virginia</h1>
           <article>
             <h2>Permitting in the City of Virginia Beach</h2>
-            <p>{"This page is still in progress. We will have more city specific information for you in the very near future. In the meantime, please reach out to us via our "}<a href='#contactUs'>Contact Us</a>{" form."}</p>
+            <p>{'The City of Virginia Beach "issues building permits for the installation, repair, upgrade, and closure of underground and aboveground storage tanks", according to their website '}
+            <a target={'_blank'} rel='noreferrer' href="https://www.vbgov.com/government/departments/planning/permits-inspections/pages/underground-aboveground-storage-tanks.aspx">{"Underground & Aboveground Storage Tanks - Permits & Inspections FAQs"}</a>
+            {". However, local contractors and city inspectors have pointed out that permitting for an aboveground storage tank removal is not actually required."} </p>
+
+            <img
+              id='virginiaBeach'
+              src='/static/city_virginiaBeach.jpg'
+              alt='Statue of Neptune in Virginia Beach.'
+            />
+            <small>King Neptune, Bronze, Virginia Beach, Virginia.</small>
+
+            <p>{"Virginia Beach inspections requires that tanks located within approximately 3.5 feet from the foundation to a structure be filled with a concrete slurry. These tanks cannot be pulled as it could be a risk to the structural integrity of the on-site structure, possibly resulting in cracks to the foundation or exterior walls. Those tanks not within the 3.5 foot zone can be pulled from the ground or, if the homeowner is unwilling to remove due to a tank being beneath a paved area or patio, can be filled with a concrete slurry."}</p>
 
             <p>{"If you have a heating oil tank that is currently not-in-use, or you suspect that your tank may be leaking fuel, we can help you find a contractor who can provide options for tank abatement at a low cost. Feel free to leave your information in the "}<a href='#contactUs'>Contact Us</a>{" form with any questions or concerns regarding your oil tank and we will be able to assist you within one business day or less."}</p>
           </article>
