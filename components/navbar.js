59f89b03cf333fa5b8d9a32cc5269a850f865c24
Joe Gonzalez <eojhet@gmail.com>
2022-05-02 22:28:00 -0400

    Changed description and updated nav w/links

--- components/navbar.js
+++ components/navbar.js
@@ -1,23 +1,58 @@
 import styles from '/styles/Navbar.module.scss';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+const links = [
+  ['/chesapeake', 'Chesapeake'],
+  ['/hampton', 'Hampton'],
+  ['/newportnews', 'Newport News'],
+  ['/norfolk', 'Norfolk'],
+  ['/portsmouth', 'Portsmouth'],
+  ['/suffolk', 'Suffolk'],
+  ['/virginiabeach', 'Virginia Beach'],
+]
 
 export default function Navbar () {
   const router = useRouter();
 
+  const LinkList = () => {
+    const linkItems = links.map((link, index) => 
+      <LinkItem key={index} link={link} />
+    );
+
+    return (
+      <ul className={styles.citiesList}>
+        {linkItems}
+      </ul>
+    )
+  }
+
+  const LinkItem = ({link}) => {
+    const style = router.pathname === link[0] ? styles.curLink : styles.link;
+    return (
+      <Link href={link[0]} passHref><li className={style}>{link[1]}</li></Link>
+    )
+  }
+
   return (
     <div className={styles.container}>
       <nav className={styles.nav}>
-        <img 
-          src="/static/ChesapeakeBayOilOpt.svg" 
-          width={200}
-          className={styles.svg}
-          alt="Chesapeake Bay Oil logo"
-          onClick={() => router.push('/')}
-        />
-        <div className={styles.headline}>
-          <h2>Chesapeake Bay Oil</h2>
-          <h3><i>Let us help you solve your heating oil tank issues.</i></h3>
+        <div className={styles.banner}>
+          <img 
+            src="/static/ChesapeakeBayOilOpt.svg" 
+            width={200}
+            className={styles.svg}
+            alt="Chesapeake Bay Oil logo"
+            onClick={() => router.push('/')}
+          />
+          <div className={styles.headline}>
+            <h2>Chesapeake Bay Oil</h2>
+            <h3><i>Let us help you solve your heating oil tank issues.</i></h3>
+          </div>
         </div>
+        <div className={styles.linkContainer}>
+          <LinkList />      
+        </div> 
       </nav>
     </div>
   )
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="Heating oil tank spills, leaks, removals, abatements, number 2 fuel oil, petroleum smell, dead grass, fill port, vent line, subsurface contamination, flooded yard, heavy rain, Hampton Roads, Norfolk, Chesapeake, Virginia Beach, Suffolk, Newport News, Hampton, Portsmouth, Smithfield, Poquoson, Eastern Shore, Cape Charles, Eastville, Machipongo, Windsor, Franklin, Smithfield, Isle of Wight, Carrolton, York, Yorktown, Williamsburg, James City." />
+        <meta name="description" content="Information about underground and aboveground heating oil tank removals. UST and AST spills, leaks, removals, abatements in Hampton Roads, Norfolk, Chesapeake, Virginia Beach, Suffolk, Newport News, Hampton, Portsmouth, Smithfield, Poquoson, Eastern Shore, Cape Charles, Eastville, Machipongo, Windsor, Franklin, Smithfield, Isle of Wight, Carrolton, York, Yorktown, Williamsburg, James City." />
         <meta charSet="UTF-8" />
         <meta property='og:locale' content='en_US' />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
